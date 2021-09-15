bc88bd78c435cd99d692e69980b294761e7667a5
Jan Reiss <jan@behead.de>
2021-09-15 22:37:35 +0200

    fix: code standarts

--- ffxiv.py
+++ ffxiv.py
@@ -17,39 +17,64 @@ import urllib.request
 import json
 import struct
 import os
-#from scapy.all import *
+
+# from scapy.all import *
 from scapy.layers.inet import TCP
-from scapy.fields import ByteField, LEShortField, LEIntField, IEEEFloatField, XLEShortField, LEShortEnumField, LEFieldLenField, XLEIntField, XShortField, PacketListField, LELongField, XByteField, ConditionalField
+from scapy.fields import (
+    ByteField,
+    LEShortField,
+    LEIntField,
+    IEEEFloatField,
+    XLEShortField,
+    LEShortEnumField,
+    LEFieldLenField,
+    XLEIntField,
+    XShortField,
+    PacketListField,
+    LELongField,
+    XByteField,
+    ConditionalField,
+)
 from scapy.packet import Packet, bind_layers
 from scapy.compat import Any
 
 
 # generate enum lists for FFXIV_IPC Types
-with urllib.request.urlopen("https://raw.githubusercontent.com/karashiiro/FFXIVOpcodes/master/opcodes.min.json") as url:
+with urllib.request.urlopen(
+    "https://raw.githubusercontent.com/karashiiro/FFXIVOpcodes/master/opcodes.min.json"
+) as url:
     opcodes = json.loads(url.read().decode())
 
     ServerZoneIpcType = {}
-    for x in opcodes[0]["lists"]["ServerZoneIpcType"]: # 0 = Global client region, 1 = CN, 2 = KR
+    for x in opcodes[0]["lists"][
+        "ServerZoneIpcType"
+    ]:  # 0 = Global client region, 1 = CN, 2 = KR
         ServerZoneIpcType[x["opcode"]] = x["name"]
 
-
     ServerLobbyIpcType = {}
-    for x in opcodes[0]["lists"]["ServerLobbyIpcType"]: # 0 = Global client region, 1 = CN, 2 = KR
+    for x in opcodes[0]["lists"][
+        "ServerLobbyIpcType"
+    ]:  # 0 = Global client region, 1 = CN, 2 = KR
         ServerLobbyIpcType[x["opcode"]] = x["name"]
 
-
     ClientZoneIpcType = {}
-    for x in opcodes[0]["lists"]["ClientZoneIpcType"]: # 0 = Global client region, 1 = CN, 2 = KR
+    for x in opcodes[0]["lists"][
+        "ClientZoneIpcType"
+    ]:  # 0 = Global client region, 1 = CN, 2 = KR
         ClientZoneIpcType[x["opcode"]] = x["name"]
 
-
     ClientLobbyIpcType = {}
-    for x in opcodes[0]["lists"]["ClientLobbyIpcType"]: # 0 = Global client region, 1 = CN, 2 = KR
+    for x in opcodes[0]["lists"][
+        "ClientLobbyIpcType"
+    ]:  # 0 = Global client region, 1 = CN, 2 = KR
         ClientLobbyIpcType[x["opcode"]] = x["name"]
 
-    joined_list = ServerZoneIpcType | ServerLobbyIpcType | ClientZoneIpcType | ClientLobbyIpcType
+    joined_list = (
+        ServerZoneIpcType | ServerLobbyIpcType | ClientZoneIpcType | ClientLobbyIpcType
+    )
 
 # The Packet dissector class
+
 
 class FFXIV_ActorMove(Packet):
     """[summary]
@@ -57,18 +82,21 @@ class FFXIV_ActorMove(Packet):
     Args:
         Packet ([type]): [description]
     """
+
     name = "FFXIV_ActorMove"
-    fields_desc=[ByteField("headRotation",          None),
-                 ByteField("rotation",              None),
-                 ByteField("animationType",         None),
-                 ByteField("animationState",        None),
-                 ByteField("animationSpeed",        None),
-                 ByteField("unknownRotation",       None),
-                 LEShortField("X",                  None),
-                 LEShortField("Y",                  None),
-                 LEShortField("Z",                  None),
-                 LEIntField("Unknown1",             None),
-                 ]
+    fields_desc = [
+        ByteField("headRotation", None),
+        ByteField("rotation", None),
+        ByteField("animationType", None),
+        ByteField("animationState", None),
+        ByteField("animationSpeed", None),
+        ByteField("unknownRotation", None),
+        LEShortField("X", None),
+        LEShortField("Y", None),
+        LEShortField("Z", None),
+        LEIntField("Unknown1", None),
+    ]
+
 
 class FFXIV_ActorCast(Packet):
     """[summary]
@@ -76,20 +104,23 @@ class FFXIV_ActorCast(Packet):
     Args:
         Packet ([type]): [description]
     """
+
     name = "FFXIV_ActorCast"
-    fields_desc=[LEShortField("Action",             None),
-                 ByteField("SkillType",             None),
-                 ByteField("Unknown1",              None),
-                 LEIntField("ItemID",               None),
-                 IEEEFloatField("CastTime",         None),
-                 LEIntField("TargetID",             None),
-                 IEEEFloatField("Rotation",         None),
-                 LEIntField("Unknown2",             None),
-                 LEShortField("posX",               None),
-                 LEShortField("posY",               None),
-                 LEShortField("posZ",               None),
-                 LEShortField("Unknown3",           None)
-                 ]
+    fields_desc = [
+        LEShortField("Action", None),
+        ByteField("SkillType", None),
+        ByteField("Unknown1", None),
+        LEIntField("ItemID", None),
+        IEEEFloatField("CastTime", None),
+        LEIntField("TargetID", None),
+        IEEEFloatField("Rotation", None),
+        LEIntField("Unknown2", None),
+        LEShortField("posX", None),
+        LEShortField("posY", None),
+        LEShortField("posZ", None),
+        LEShortField("Unknown3", None),
+    ]
+
 
 class FFXIV_ActorControl(Packet):
     """[summary]
@@ -97,15 +128,18 @@ class FFXIV_ActorControl(Packet):
     Args:
         Packet ([type]): [description]
     """
+
     name = "FFXIV_ActorControl"
-    fields_desc=[LEShortField("Type",              None),
-                 LEShortField("Unknown1",          None),
-                 LEIntField("Data0",               None),
-                 LEIntField("Data1",               None),
-                 LEIntField("Data2",               None),
-                 LEIntField("Data3",               None),
-                 LEIntField("Data4",               None)
-                 ]
+    fields_desc = [
+        LEShortField("Type", None),
+        LEShortField("Unknown1", None),
+        LEIntField("Data0", None),
+        LEIntField("Data1", None),
+        LEIntField("Data2", None),
+        LEIntField("Data3", None),
+        LEIntField("Data4", None),
+    ]
+
 
 class FFXIV_UpdateHpMpTp(Packet):
     """[summary]
@@ -113,11 +147,13 @@ class FFXIV_UpdateHpMpTp(Packet):
     Args:
         Packet ([type]): [description]
     """
+
     name = "FFXIV_UpdateHpMpTp"
-    fields_desc=[LEIntField("HP",               None),
-                 LEShortField("MP",             None),
-                 LEShortField("TP",             None),
-                 ]
+    fields_desc = [
+        LEIntField("HP", None),
+        LEShortField("MP", None),
+        LEShortField("TP", None),
+    ]
 
 
 class FFXIV_UpdatePositionHandler(Packet):
@@ -126,18 +162,17 @@ class FFXIV_UpdatePositionHandler(Packet):
     Args:
         Packet ([type]): [description]
     """
+
     name = "FFXIV_UpdatePositionHandler"
-    fields_desc=[LEIntField("rot",              None),
-                 LEIntField("2",                None),
-                 LEIntField("x",                None),
-                 LEIntField("z",                None),
-                 LEIntField("y",                None),
-                 LEIntField("6",                None),
-                 LEIntField("7",                None),
-                 LEIntField("8",                None),
-                 LEIntField("9",                None),
-                 LEIntField("10",               None),
-                 ]
+    fields_desc = [
+        LEIntField("rot", None),
+        LEIntField("2", None),
+        LEIntField("x", None),
+        LEIntField("z", None),
+        LEIntField("y", None),
+        LEIntField("6", None),
+    ]
+
 
 class FFXIV_IPC(Packet):
     """[IPC Opcode Multiplexer]
@@ -145,14 +180,17 @@ class FFXIV_IPC(Packet):
     Args:
         Packet ([Packet]): [raw Packet stripped by FFXIV_Segment]
     """
+
     name = "FFXIV_IPC"
-    fields_desc=[XLEShortField("ipc_magic",         None),
-                 LEShortEnumField("ipc_type",       None, joined_list),
-                 XLEShortField("ipc_unknown1",      None),
-                 XLEShortField("ipc_server_id",     None),
-                 LEIntField("ipc_epoch",            None),
-                 XLEIntField("ipc_unknown2",        None),
-                 ]
+    fields_desc = [
+        XLEShortField("ipc_magic", None),
+        LEShortEnumField("ipc_type", None, joined_list),
+        XLEShortField("ipc_unknown1", None),
+        XLEShortField("ipc_server_id", None),
+        LEIntField("ipc_epoch", None),
+        XLEIntField("ipc_unknown2", None),
+    ]
+
 
 class FFXIV_ClientKeepAlive(Packet):
     """[recurring keepalive Packet]
@@ -160,10 +198,10 @@ class FFXIV_ClientKeepAlive(Packet):
     Args:
         Packet ([Packet]): [raw Packet stripped by FFXIV_Segment]
     """
+
     name = "FFXIV_ClientKeepAlive"
-    fields_desc=[LEIntField("ID",                   None),
-                 LEIntField("Epoch",                None)
-                 ]
+    fields_desc = [LEIntField("ID", None), LEIntField("Epoch", None)]
+
 
 class FFXIV_ServerKeepAlive(Packet):
     """[recurring keepalive Packet]
@@ -171,10 +209,10 @@ class FFXIV_ServerKeepAlive(Packet):
     Args:
         Packet ([Packet]): [raw Packet stripped by FFXIV_Segment]
     """
+
     name = "FFXIV_ServerKeepAlive"
-    fields_desc=[LEIntField("ID",                   None),
-                 LEIntField("Epoch",                None)
-                 ]
+    fields_desc = [LEIntField("ID", None), LEIntField("Epoch", None)]
+
 
 class FFXIV_Segment(Packet):
     """[segments the raw packet]
@@ -182,16 +220,41 @@ class FFXIV_Segment(Packet):
     Args:
         Packet ([Packet]): [raw packet stripped by FFXIV]
     """
+
     name = "FFXIV_Segment"
-    fields_desc=[ LEFieldLenField("Size",    None, length_of="data",fmt="<I"),
-                  XLEIntField("Source",      None),
-                  XLEIntField("Target",      None),
-                  LEShortEnumField("Type",   None, {3:"IPC",7:"ClientKeepAlive",8:"ServerKeepAlive"}),
-                  XShortField("Unknown",     None),
-                  ConditionalField(PacketListField("data",    None, FFXIV_ServerKeepAlive, length_from = lambda pkt: pkt.Size -16) , lambda pkt: pkt.Type == 8),
-                  ConditionalField(PacketListField("data",    None, FFXIV_ClientKeepAlive, length_from = lambda pkt: pkt.Size -16) , lambda pkt: pkt.Type == 7),
-                  ConditionalField(PacketListField("data",    None, FFXIV_IPC, length_from = lambda pkt: pkt.Size -16)         , lambda pkt: pkt.Type == 3)
-                 ]
+    fields_desc = [
+        LEFieldLenField("Size", None, length_of="data", fmt="<I"),
+        XLEIntField("Source", None),
+        XLEIntField("Target", None),
+        LEShortEnumField(
+            "Type", None, {3: "IPC", 7: "ClientKeepAlive", 8: "ServerKeepAlive"}
+        ),
+        XShortField("Unknown", None),
+        ConditionalField(
+            PacketListField(
+                "data",
+                None,
+                FFXIV_ServerKeepAlive,
+                length_from=lambda pkt: pkt.Size - 16,
+            ),
+            lambda pkt: pkt.Type == 8,
+        ),
+        ConditionalField(
+            PacketListField(
+                "data",
+                None,
+                FFXIV_ClientKeepAlive,
+                length_from=lambda pkt: pkt.Size - 16,
+            ),
+            lambda pkt: pkt.Type == 7,
+        ),
+        ConditionalField(
+            PacketListField(
+                "data", None, FFXIV_IPC, length_from=lambda pkt: pkt.Size - 16
+            ),
+            lambda pkt: pkt.Type == 3,
+        ),
+    ]
 
     def extract_padding(self, s):
         """[the key to multi segments in one bundle]
@@ -202,7 +265,8 @@ class FFXIV_Segment(Packet):
         Returns:
             [Packet]: [Only returns the packet but does not consumes the rest of the incomming packet.]
         """
-        return '', s
+        return "", s
+
 
 class FFXIV(Packet):
     """[base Class]
@@ -213,23 +277,27 @@ class FFXIV(Packet):
     Returns:
         [None]: [description]
     """
+
     name = "FFXIV"
-    fields_desc=[ XLEIntField("magic0",        None),
-                  XLEIntField("magic1",        None),
-                  XLEIntField("magic2",        None),
-                  XLEIntField("magic3",        None),
-                  LELongField("epoch",         None),
-                  LEFieldLenField("bundle_len",None),
-                  XLEShortField("unknown1",    None),
-                  XLEShortField("conn_type",   None),
-                  LEFieldLenField("msg_count", None, count_of="data"),
-                  XByteField("encoding",       None),
-                  ByteField("compressed",      None),
-                  XLEShortField("unknown3" ,   None),
-                  XLEShortField("unknown4" ,   None),
-                  XLEShortField("unknown5" ,   None),
-                  PacketListField("data",      None, FFXIV_Segment, count_from = lambda pkt: pkt.msg_count)
-                 ]
+    fields_desc = [
+        XLEIntField("magic0", None),
+        XLEIntField("magic1", None),
+        XLEIntField("magic2", None),
+        XLEIntField("magic3", None),
+        LELongField("epoch", None),
+        LEFieldLenField("bundle_len", None),
+        XLEShortField("unknown1", None),
+        XLEShortField("conn_type", None),
+        LEFieldLenField("msg_count", None, count_of="data"),
+        XByteField("encoding", None),
+        ByteField("compressed", None),
+        XLEShortField("unknown3", None),
+        XLEShortField("unknown4", None),
+        XLEShortField("unknown5", None),
+        PacketListField(
+            "data", None, FFXIV_Segment, count_from=lambda pkt: pkt.msg_count
+        ),
+    ]
 
     @classmethod
     def tcp_reassemble(cls, data, metadata):
@@ -245,6 +313,7 @@ class FFXIV(Packet):
         length = struct.unpack("<I", data[24:28])[0]
         if len(data) == length:
             return FFXIV(data)
+
 
 bind_layers(TCP, FFXIV, sport=54993)
 bind_layers(TCP, FFXIV, dport=54993)
--- pcap.py
+++ pcap.py
@@ -4,7 +4,11 @@ from collections import deque
 from functools import partial
 from time import sleep, time
 from threading import Thread
-from scapy.all import sniff, Packet, TCPSession
+from scapy.all import sniff, Packet, TCPSession, conf
+from scapy.layers.l2 import Ether
+from scapy.layers.inet import IP, TCP
+from scapy.packet import Raw
+
 from ffxiv import FFXIV_IPC, FFXIV, FFXIV_UpdatePositionHandler, FFXIV_Segment
 
 log = logging.getLogger(__name__)
@@ -13,43 +17,55 @@ log.setLevel(logging.INFO)
 
 import urllib.request, json
 
+conf.layers.filter([Ether, IP, TCP, FFXIV])
+
 # generate enum lists for FFXIV_IPC Types
-with urllib.request.urlopen("https://raw.githubusercontent.com/karashiiro/FFXIVOpcodes/master/opcodes.min.json") as url:
+with urllib.request.urlopen(
+    "https://raw.githubusercontent.com/karashiiro/FFXIVOpcodes/master/opcodes.min.json"
+) as url:
     opcodes = json.loads(url.read().decode())
 
     ServerZoneIpcType = {}
-    for x in opcodes[0]["lists"]["ServerZoneIpcType"]: # 0 = Global client region, 1 = CN, 2 = KR
+    for x in opcodes[0]["lists"][
+        "ServerZoneIpcType"
+    ]:  # 0 = Global client region, 1 = CN, 2 = KR
         ServerZoneIpcType[x["opcode"]] = x["name"]
 
-
     ServerLobbyIpcType = {}
-    for x in opcodes[0]["lists"]["ServerLobbyIpcType"]: # 0 = Global client region, 1 = CN, 2 = KR
+    for x in opcodes[0]["lists"][
+        "ServerLobbyIpcType"
+    ]:  # 0 = Global client region, 1 = CN, 2 = KR
         ServerLobbyIpcType[x["opcode"]] = x["name"]
 
-
     ClientZoneIpcType = {}
-    for x in opcodes[0]["lists"]["ClientZoneIpcType"]: # 0 = Global client region, 1 = CN, 2 = KR
+    for x in opcodes[0]["lists"][
+        "ClientZoneIpcType"
+    ]:  # 0 = Global client region, 1 = CN, 2 = KR
         ClientZoneIpcType[x["opcode"]] = x["name"]
 
-
     ClientLobbyIpcType = {}
-    for x in opcodes[0]["lists"]["ClientLobbyIpcType"]: # 0 = Global client region, 1 = CN, 2 = KR
+    for x in opcodes[0]["lists"][
+        "ClientLobbyIpcType"
+    ]:  # 0 = Global client region, 1 = CN, 2 = KR
         ClientLobbyIpcType[x["opcode"]] = x["name"]
 
-    joined_list = ServerZoneIpcType | ServerLobbyIpcType | ClientZoneIpcType | ClientLobbyIpcType
+    joined_list = (
+        ServerZoneIpcType | ServerLobbyIpcType | ClientZoneIpcType | ClientLobbyIpcType
+    )
 
 
 packets = deque()
 
+
 def add_packet_to_queue(pkt: Packet):
-    """ Operates on each sniffed packet
-        Adds packet to the Queue for background processing
+    """Operates on each sniffed packet
+    Adds packet to the Queue for background processing
     """
     packets.append(pkt)
 
 
 def poll_packet_queue(token: str):
-    """  Background task to poll the Packets queue """
+    """Background task to poll the Packets queue"""
     while True:
         queue_size = len(packets)
         if queue_size > 0 and queue_size % 20 == 0:
@@ -62,7 +78,7 @@ def poll_packet_queue(token: str):
 
         raw_packet = packets.popleft()
         try:
-            #print(f"{raw_packet.summary()}")
+            # print(f"{raw_packet.summary()}")
             if raw_packet.haslayer(FFXIV_IPC):
                 if raw_packet[FFXIV_IPC].ipc_type in joined_list.keys():
                     pdfpath = f"PDFs/IPC_{raw_packet[FFXIV_IPC].ipc_type}_{joined_list[raw_packet[FFXIV_IPC].ipc_type]}.pdf"
@@ -70,18 +86,22 @@ def poll_packet_queue(token: str):
                     pdfpath = f"PDFs/IPC_{raw_packet[FFXIV_IPC].ipc_type}.pdf"
                 if not os.path.isfile(pdfpath):
                     raw_packet[FFXIV_IPC].pdfdump(pdfpath)
-                    print()
 
-            if raw_packet.haslayer(FFXIV_UpdatePositionHandler): # and raw_packet[FFXIV].msg_count > 1:
-                posX = raw_packet["FFXIV_UpdatePositionHandler"].x
-                posY = raw_packet["FFXIV_UpdatePositionHandler"].y
-                posZ = raw_packet["FFXIV_UpdatePositionHandler"].z
-                print(f"X: {posX} Y: {posY} Z: {posZ}")
+            # if raw_packet.haslayer(FFXIV_UpdatePositionHandler): # and raw_packet[FFXIV].msg_count > 1:
+            #    posX = raw_packet["FFXIV_UpdatePositionHandler"].x
+            #    posY = raw_packet["FFXIV_UpdatePositionHandler"].y
+            #    posZ = raw_packet["FFXIV_UpdatePositionHandler"].z
+            #    print(f"X: {posX} Y: {posY} Z: {posZ}")
+
             if raw_packet.haslayer(FFXIV):
-                result = (f"{raw_packet.show(dump=True)}")
+                result = f"{raw_packet.show(dump=True)}"
                 for item in result.split("\n"):
                     if "ipc_type" in item:
-                        print (item.strip())
+                        print(item.strip())
+
+            if raw_packet.haslayer(Raw):
+                raw_packet.show()
+
         except:
             log.exception(f"Failed to parse: {raw_packet.summary()}")
             continue
@@ -96,7 +116,12 @@ if __name__ == "__main__":
     log.info(f"Started queue polling")
 
     log.info(f"Sniffing packets... Ctrl + C to stop sniffing")
-    sniff(filter="tcp and net (195.82.50.0/24 or 204.2.229.0/24 or 124.150.157.0/24)", prn=add_packet_to_queue, store=0, session=TCPSession)
+    sniff(
+        filter="tcp and net (195.82.50.0/24 or 204.2.229.0/24 or 124.150.157.0/24)",
+        prn=add_packet_to_queue,
+        store=0,
+        session=TCPSession,
+    )
     # 195.82.50.0/24 Europe
     # 204.2.229.0/24 NA
     # 124.150.157.0/24 Japan
